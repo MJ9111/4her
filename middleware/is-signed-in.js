748191904b45bf64bd17320d12f3eb72a9cc8e20
mj9111 <1191.1mj91.1@gmail.com>
2024-07-28 15:14:10 +0300

    first commit

---
+++ middleware/is-signed-in.js
@@ -0,0 +1,15 @@
+const isSignedIn = (req, res, next) => {
+    if (req.session.user) return next();
+    res.redirect('/auth/sign-in');
+  };
+  
+  module.exports = isSignedIn;
+
+
+  
+
+/**
+ req :is the request object.
+ res :is the response object.
+ next:is the third parameter, representing the next function in the long line of middleware and route handlers that a request is processed through.
+ */
---
+++ middleware/pass-user-to-view.js
@@ -0,0 +1,14 @@
+const passUserToView = (req, res, next) => {
+    res.locals.user = req.session.user ? req.session.user : null
+    next()
+}
+
+module.exports = passUserToView
+
+
+
+/**
+ req :is the request object.
+ res :is the response object.
+ next:is the third parameter, representing the next function in the long line of middleware and route handlers that a request is processed through.
+ */
